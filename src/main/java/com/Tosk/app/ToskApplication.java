096816b475f21d386ddb6d723ac03428f521a5a3
Rujuu <n.ujiie2118@gmail.com>
2025-08-21 04:57:08 +0900

    :art: correct package name casing and improve main method parameter declaration

--- src/main/java/com/Tosk/app/ToskApplication.java
+++ src/main/java/com/Tosk/app/ToskApplication.java
@@ -1,4 +1,4 @@
-package com.Tosk.app;
+package com.tosk.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /** Entry point for the Tosk application. */
 @SpringBootApplication
 public class ToskApplication {
-  public static void main(String[] args) {
+  public static void main(final String[] args) {
     SpringApplication.run(ToskApplication.class, args);
   }
 }
