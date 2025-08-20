a4662aae41a83da123a288939cd7138fbeb2925a
Rujuu <n.ujiie2118@gmail.com>
2025-08-21 04:46:22 +0900

    :art: format ToskApplication.java for improved readability

--- src/main/java/com/Tosk/app/ToskApplication.java
+++ src/main/java/com/Tosk/app/ToskApplication.java
@@ -3,12 +3,10 @@ package com.Tosk.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Entry point for the Tosk application.
- */
+/** Entry point for the Tosk application. */
 @SpringBootApplication
 public class ToskApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(ToskApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(ToskApplication.class, args);
+  }
 }
